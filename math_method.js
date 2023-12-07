03e92ad0ba6a749d053b253e6ed0a44f70d4f9b7
vijay solanki <vijaysolanki1481@gmail.com>
2023-12-07 09:32:29 +0530

    07 dec

---
+++ math_method.js
@@ -0,0 +1,44 @@
+// Maths Object property and method
+
+// constant
+// console.log(Math.E)
+// console.log(Math.PI)
+// console.log(Math.SQRT2)
+// console.log(Math.SQRT1_2)
+// console.log(Math.LN10)
+// console.log(Math.LN2)
+// console.log(Math.LOG10E)
+// console.log(Math.LOG2E)
+
+
+// console.log(Math.abs(12))       // absolute value
+// console.log(Math.abs(-122))       
+// console.log(Math.ceil(3.6))       // round up value
+// console.log(Math.ceil(-3.2))         
+// console.log(Math.floor(3.6))       // round down value
+// console.log(Math.floor(-3.2))       
+// console.log(Math.round(3.6))       // rounded value
+// console.log(Math.round(-3.2))          
+// console.log(Math.sqrt(16))       // square root
+// console.log(Math.sqrt(5.5))          
+// console.log(Math.pow(2,5))       // x to power y
+// console.log(Math.pow(5.5,2))          
+// console.log(Math.trunc(2.1))       // interger number
+// console.log(Math.trunc(-22.5))          
+// console.log(Math.random()*30)       // random number (0-1)
+// console.log(Math.max(10,25,90,1,25,33))       // max number 
+// console.log(Math.min(10,25,90,1,25,33))       // min number 
+// console.log(Math.sign(3.6))       // sign check
+// console.log(Math.sign(-3.2))
+// console.log(Math.sin(45 * Math.PI/180))       
+// console.log(Math.sin(0))
+// console.log(Math.cos(30 * Math.PI/180))       
+// console.log(Math.cos(0))
+// console.log(Math.tan(30 * Math.PI/180))       
+// console.log(Math.tan(0))
+// console.log(Math.log(30))       
+// console.log(Math.log(1))
+// console.log(Math.log10(100))        
+// console.log(Math.log10(10)) 
+// console.log(Math.log2(32))        
+// console.log(Math.log2(2)) 
