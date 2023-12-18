ad0e200fa73dc2d55a8d0ee175972040ed49bf79
vijay solanki <vijaysolanki1481@gmail.com>
2023-12-18 09:36:32 +0530

    18-Dec

---
+++ class_object.js
@@ -0,0 +1,61 @@
+// // Object and class
+
+// let person ={
+//     age:25,
+//     greet: function(){
+//         return("hello good moring")
+//     }
+// };
+
+// person.name="skill"  // add to property
+// person.nationality="indian"
+
+// console.log(person)
+// console.log(pereson.age)
+// console.log(person.greet()) 
+
+// delete person.age
+// console.log(person)
+
+//   Object access property
+// console.log(person.age)
+// console.log(person["age"]) 
+
+// let pereson={
+//     name:"vijay",
+//     age:23,
+//     marks:{
+//         maths:90,
+//         science:85,
+//         english:76
+//     },
+//     hobbies:["reading","dancing","photography"]
+// };
+
+// console.log(person)
+// console.log(person.mark)
+// console.log(person.marks.maths)
+// console.log(person,hobbies[1])
+
+
+// class person{
+//     constructor(name,city){
+//         this.name=name
+//         this.city=city
+//     }
+//     greet(){
+//         return ("Hello I am "+this.name+"\n from"+this.city)
+//     }
+// }
+
+//    let vijay=new person("VIJAY","surat")
+//    console.log(vijay.great())
+
+
+// // template literal
+
+// let a=25,b=50
+
+// let data=`hello good morning
+// ${a*b} ${b}`
+// console.log(data)
